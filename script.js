1d63a3b6a451183d81b51bfd2db8cf2ad31b428b
Ariana <133261064+Arianaff0@users.noreply.github.com>
2023-12-24 23:53:32 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+//script.js
+
+function redirectToHome() {
+    window.location.href = 'home.html';
+}
+function redirectToConverter() {
+    window.location.href = 'converter.html';
+}
+function redirectToAbout() {
+    window.location.href = 'about.html';
+}
+function redirectToContact() {
+    window.location.href = 'contact.html';
+}
+
+
+function triggerBtnClick() {
+    document.getElementById('About').click();
+}
