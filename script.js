e002f819e52663358249cda6c70758c4e20230c7
Arjun Sharma <ashar562@uwo.ca>
2024-01-11 18:14:41 -0500

    Fix about page image styling and standardize styling

--- script.js
+++ script.js
@@ -3,12 +3,15 @@
 function redirectToHome() {
     window.location.href = 'home.html';
 }
+
 function redirectToConverter() {
     window.location.href = 'converter.html';
 }
+
 function redirectToAbout() {
     window.location.href = 'about.html';
 }
+
 function redirectToContact() {
     window.location.href = 'contact.html';
 }
